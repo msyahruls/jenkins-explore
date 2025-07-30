beb40d0d907486726292a8013a160c42f77b4aa8
Syahrul Setiawan <msyahruls-noreply@github.com>
2025-07-30 15:32:17 +0700

    chore: update pipeline

--- pipeline.sh
+++ pipeline.sh
@@ -1,21 +1,41 @@
 pipeline{
   agent any
+  triggers {
+    githubPush()
+  }
   stages {
+    stage('Check Branch') {
+      when {
+        branch 'development'
+      }
+
+      steps {
+        echo 'Not on development branch — skipping pipeline.'
+        script {
+          currentBuild.result = 'SUCCESS'
+          return
+        }
+      }
+    }
+
     stage('Checkout') {
       steps {
         git 'https://github.com/msyahruls/jenkins-explore.git'
       }
     }
+
     stage('Build') {
       steps {
         sh 'go build main.go'
       }
     }
+
     stage('Test') {
       steps {
         sh 'go test ./...'
       }
     }
+
     stage('Archive Binary') {
       steps {
         archiveArtifacts artifacts: 'app', fingerprint: true
